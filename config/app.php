807901e0c8916d1a6ae54cb526e912868c228f5d
solisaponte <ndrs.sa.cr@gmail.com>
2018-10-04 19:41:49 -0600

    cambio datasource

--- config/app.php
+++ config/app.php
@@ -255,8 +255,8 @@ return [
              * the following line and set the port accordingly
              */
             'port' => '22',
-            'username' => 'Jose Luis',
-            'password' => '',
+            'username' => 'Andres',
+            'password' => 'B26505',
             'database' => 'proyecto_inge',
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
