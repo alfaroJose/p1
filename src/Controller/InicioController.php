5a1d0fc870aefaac7e9701f43ef592db0d537425
analaura19 <analaura19mongesoto@gmail.com>
2018-10-26 09:00:24 -0600

    arreglo en el botón de editar perfil
    Cuando el usuario se está registrando por primera vez no hay opción de editar perfil todavía

--- src/Controller/InicioController.php
+++ src/Controller/InicioController.php
@@ -72,7 +72,7 @@ class InicioController extends AppController
                 //aquí se verifica si el usuario existe en la tabla o no para mandarlo a la vista principal o de añadir sus datos personales
                 $users = TableRegistry::get('Usuarios');
                 $index = $users->find()
-                ->select(['nombre_usuario'])
+                ->select(['id'])
                 ->where(['nombre_usuario =' => $usuario])
                 ->toList();
 
