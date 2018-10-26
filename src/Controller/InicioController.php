fc312a28b2ce3c04b94920e14c824e0ec880660e
analaura19 <analaura19mongesoto@gmail.com>
2018-10-26 08:48:50 -0600

    cambio en la consulta

--- src/Controller/InicioController.php
+++ src/Controller/InicioController.php
@@ -72,7 +72,7 @@ class InicioController extends AppController
                 //aquí se verifica si el usuario existe en la tabla o no para mandarlo a la vista principal o de añadir sus datos personales
                 $users = TableRegistry::get('Usuarios');
                 $index = $users->find()
-                ->select(['id'])
+                ->select(['nombre_usuario'])
                 ->where(['nombre_usuario =' => $usuario])
                 ->toList();
 
