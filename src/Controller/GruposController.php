41d3b886f9e5cf582fc3aebe429d38f4212e101c
Sergio <sergio.tijerino@ucr.ac.cr>
2018-10-28 20:06:33 -0600

    Seguridad implementada en el controlador de Cursos

--- src/Controller/GruposController.php
+++ src/Controller/GruposController.php
@@ -3,6 +3,8 @@ namespace App\Controller;
 
 use App\Controller\AppController;
 use Cake\Datasource\ConnectionManager;
+use Cake\ORM\TableRegistry;
+
 
 /**
  * Grupos Controller
@@ -21,6 +23,28 @@ class GruposController extends AppController
      */
     public function index()
     {
+        //Verifica por permisos y login
+        $carne = $this->getRequest()->getSession()->read('id'); 
+        if($carne != null){
+           $connect = ConnectionManager::get('default');
+           $consulta = "select roles_id from usuarios where nombre_usuario = '".$carne."';";
+           $rol =  $connect->execute($consulta)->fetchAll(); //Devuelve el rol del usuario en cuestión
+          
+           $consulta = "select pos.estado
+                       from posee as pos join permisos as per on pos.permisos_id =  per.id
+                        where per.id = 1 and roles_id = ".$rol[0][0].";";
+                        //1 = Consultar Curso-Grupo
+           $tupla =  $connect->execute($consulta)->fetchAll();      
+
+            if($tupla[0][0] != '1'){//1 = Tiene permisos para consultar usuarios
+               $this->redirect(['controller' => 'Inicio','action' => 'fail']);
+            }
+        }
+        else{
+            $this->redirect(['controller' => 'Inicio','action' => 'fail']);
+        }
+        //Cierra la seguridad
+
         $todo= $this->Grupos->getIndexValues();
         $this->paginate = [
             'contain' => ['Usuarios']
@@ -36,7 +60,7 @@ class GruposController extends AppController
      * @param string|null $id Grupo id.
      * @return \Cake\Http\Response|void
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
+     *//*
     public function view($id = null)
     {
         $grupo = $this->Grupos->get($id, [
@@ -44,7 +68,7 @@ class GruposController extends AppController
         ]);
 
         $this->set('grupo', $grupo);
-    }
+    } COMENTADO PARA EVITAR ACCESOS POR URL*/
 
     /**
      * Add method
@@ -53,6 +77,28 @@ class GruposController extends AppController
      */
     public function add()
     {
+        //Verifica por permisos y login
+        $carne = $this->getRequest()->getSession()->read('id'); 
+        if($carne != null){
+           $connect = ConnectionManager::get('default');
+           $consulta = "select roles_id from usuarios where nombre_usuario = '".$carne."';";
+           $rol =  $connect->execute($consulta)->fetchAll(); //Devuelve el rol del usuario en cuestión
+          
+           $consulta = "select pos.estado
+                       from posee as pos join permisos as per on pos.permisos_id =  per.id
+                        where per.id = 3 and roles_id = ".$rol[0][0].";";
+                        //3 = Agregar Curso-Grupo
+           $tupla =  $connect->execute($consulta)->fetchAll();      
+
+            if($tupla[0][0] != '1'){//1 = Tiene permisos para consultar usuarios
+               $this->redirect(['controller' => 'Inicio','action' => 'fail']);
+            }
+        }
+        else{
+            $this->redirect(['controller' => 'Inicio','action' => 'fail']);
+        }
+        //Cierra la seguridad
+
         $grupo = $this->Grupos->newEntity();
         if ($this->request->is('post')) {
             $grupo = $this->Grupos->patchEntity($grupo, $this->request->getData());
@@ -88,6 +134,28 @@ class GruposController extends AppController
      */
      public function edit($id = null, $id2 = null, $id3 = null)
     {
+        //Verifica por permisos y login
+        $carne = $this->getRequest()->getSession()->read('id'); 
+        if($carne != null){
+           $connect = ConnectionManager::get('default');
+           $consulta = "select roles_id from usuarios where nombre_usuario = '".$carne."';";
+           $rol =  $connect->execute($consulta)->fetchAll(); //Devuelve el rol del usuario en cuestión
+          
+           $consulta = "select pos.estado
+                       from posee as pos join permisos as per on pos.permisos_id =  per.id
+                        where per.id = 4 and roles_id = ".$rol[0][0].";";
+                        //4 = Editar Curso-Grupo
+           $tupla =  $connect->execute($consulta)->fetchAll();      
+
+            if($tupla[0][0] != '1'){//1 = Tiene permisos para consultar usuarios
+               $this->redirect(['controller' => 'Inicio','action' => 'fail']);
+            }
+        }
+        else{
+            $this->redirect(['controller' => 'Inicio','action' => 'fail']);
+        }
+        //Cierra la seguridad
+
         $encontrado=false;
         $it=0;
         $defaultSelect=0;
